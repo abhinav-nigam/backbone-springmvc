4d97aaedd7a6cd89a7531f86964e4a08fcbcf554
Abhinav Nigam <abhinav@abhinav-HP-Pavilion-dv6-Notebook-PC.(none)>
2012-01-27 13:35:17 +0530

    home page header

---
+++ src/main/webapp/resources/js/lib/JQuery.Ns.js
@@ -0,0 +1,29 @@
+(function( $ ){
+  /*
+    obj is completely optional and allows the following
+  
+    $.ns("my.whatever", function() {})
+    
+    or
+    
+    var myobj = {}
+    $.ns("my.whatever", myobj);
+  */
+  
+  $.ns = function(ns, obj) {
+    var parts = ns.split(".");
+    var root = window || {};
+    
+    for(var i=0;i<parts.length;i++) {
+      
+      if(typeof root[parts[i]] != "object") {
+        root[parts[i]] = (i == parts.length - 1 && obj) ? obj : {};
+      }
+      
+      // ensure the current root is now the current object       
+      root = root[parts[i]];
+    }
+    
+    return root;
+  };
+})( jQuery );
